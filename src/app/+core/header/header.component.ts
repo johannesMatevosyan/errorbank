531d644748ea4998a3b82faa48f2550285eed8be
johannesMatevosyan <h.matevosyan@yahoo.com>
2019-11-22 19:16:54 +0400

    - reload the current route with the angular router

--- src/app/+core/header/header.component.ts
+++ src/app/+core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from "@app/+shared/_services/auth.service";
 import { Subscription } from "rxjs/index";
 import { SearchFilterService } from "@app/+shared/_services/search-filter.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,9 @@ export class HeaderComponent implements OnInit {
   profile: any;
   userIsAuthenticated = false;
   authListenerSubs: Subscription;
-  constructor(private authService: AuthService, private searchFilterService: SearchFilterService) {}
+  constructor(private authService: AuthService,
+              private router: Router,
+              private searchFilterService: SearchFilterService) {}
 
   ngOnInit() {
     this.checkAuthenticationStatus();
@@ -51,8 +54,14 @@ export class HeaderComponent implements OnInit {
     this.searchFilterService.changeSearch(item)
   }
 
+  redirectTo(uri:string){
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+      this.router.navigate([uri]));
+  }
+
   onLogout() {
     this.authService.logout();
   }
+
 
 }
--- src/app/+dashboard/dashboard-routing.module.ts
+++ src/app/+dashboard/dashboard-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [{
   children: [
     {
       path: 'get-all',
-      component: ListPostsComponent
+      component: ListPostsComponent,
     },
     {
       path: 'create',
